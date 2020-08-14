7017296f1394d9a18925c009db5c0823f00e4d21
amit-82 <leshem.amit@gmail.com>
2020-08-14 10:56:38 +0300

    removed test text

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,16 @@
 <template>
-	<section class="layout-row justify-content-center mb-40">
-		"bla"
-		<input
-			type="text"
-			placeholder="Search for movie by name"
-			class="w-75 py-2"
-			data-testid="search"
-		/>
-	</section>
+  <section class="layout-row justify-content-center mb-40">
+    <input
+      type="text"
+      placeholder="Search for movie by name"
+      class="w-75 py-2"
+      data-testid="search"
+    />
+  </section>
 </template>
 
 <script>
 export default {
-	name: "Search",
+  name: "Search"
 };
 </script>
